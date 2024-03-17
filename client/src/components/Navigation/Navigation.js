fc818a18d5ace272cb52fdc4a63d66a9b8ee4d37
unknown <ana.popova.register@gmail.com>
2024-03-17 14:47:09 +0200

    updating navbar css

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -19,20 +19,24 @@ function Navigation(){
     return(
     <div className={styles.headerNEW}>
         <nav className={styles.navbar}>
+        <h2>Doncho Zahariev</h2>
             <img src="/images/Logo.png" alt="" className={styles.logo}/>
             <ul>
-            <li> <Link to="/">Home</Link></li>
-            <li> <Link to="/catalog">Catalog</Link></li>
+            <li> <Link to="/">home</Link></li>
+            <li> <Link to="/abstract">abstract series</Link></li>
+            <li> <Link to="/horizons">horizons series</Link></li>
+            <li> <Link to="/other">other works</Link></li>
+            <li> <Link to="/press">press</Link></li>
+            <li> <Link to="/links">links</Link></li>
+            <li> <Link to="/about">about</Link></li>
+            <li> <Link to="/contacts">contatcs</Link></li>
 
                 { isLogged ?
                  (<>
                  <li> <Link to="/create">CREATE</Link></li>
                  <li> <Link to="/myProfile">MY PROFILE</Link></li>
                  <li> <Link to="" onClick={onLogOutClick}>LOGOUT</Link></li>
-                 </>)
-                 :
-                 (<><li> <Link to="/login">Login</Link></li>
-                    <li> <Link to="/register">Register</Link></li></>)
+                 </>) : ''
                 } 
 
             </ul>

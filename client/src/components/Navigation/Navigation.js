16b90cc8bfde383d72979ef602e1ae22d7256131
unknown <ana.popova.register@gmail.com>
2024-03-18 12:46:03 +0200

    up

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,5 +1,6 @@
 import styles from "./Navigation.module.css";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import * as data from '../../api/data';
 import { Link } from "react-router-dom";
 import { useContext } from "react";
@@ -7,6 +8,7 @@ import { PaintingsContext } from "../../contexts/PaintingsContext";
 
 
 function Navigation(){
+    const [activeMenuItem, setActiveMenuItem] = useState(null);
     const {isLogged} = useContext(PaintingsContext)
 
     const navigate = useNavigate()
@@ -16,13 +18,18 @@ function Navigation(){
         navigate("/")
     }
 
+    const handleMenuItemClick = (menuItem) => {
+        setActiveMenuItem(menuItem);
+      };
+
+      console.log(activeMenuItem)
     return(
     <div className={styles.headerNEW}>
         <nav className={styles.navbar}>
         <h2><Link to="/">Doncho Zahariev</Link></h2>
             <img src="/images/Logo.png" alt="" className={styles.logo}/>
             <ul>
-            <li> <Link to="/abstract">abstract series</Link></li>
+            <li> <Link to="/abstract" className={activeMenuItem === 'abstract' ? 'bold-a' : ''} onClick={() => handleMenuItemClick('abstract')}>abstract series</Link></li>
             <li> <Link to="/horizons">horizons series</Link></li>
             <li> <Link to="/other">other works</Link></li>
             <li> <Link to="/press">press</Link></li>

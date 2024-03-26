2d15868f3aa17379794bea52cee312e276576f62
unknown <ana.popova.register@gmail.com>
2024-03-26 22:22:38 +0200

    updated catalog page

--- client/src/components/PressPage/PressPage.js
+++ client/src/components/PressPage/PressPage.js
@@ -11,8 +11,14 @@ function PressPage() {
     <main className={styles.pressPageMain}>
       <div>
       <Link to={`https://www.contemporaryartcuratormagazine.com/home-2/doncho-zahariev`}>Contemporary Art Curator Magazine</Link>
+      <Link to={`https://sbh.bg/bg/izlojben-kalendar-izlojbi-sbh/horizonti-izlojba-na-doncho-zahariev/8/1002`}>The Union of Bulgarian Artists (UBA)</Link>
+      <Link to={`https://zapernik.com/za-pernik/37606/`}>Zapernik.com</Link>
+      <Link to={`https://www.dnevnik.bg/sled5/izlojbi/2015/09/24/2616262_doncho_zahariev/`}>Dnevnik.bg</Link>
+      <Link to={`https://www.youtube.com/watch?v=y6UERL01RRk`}>SOLO EXHIBITION in Pernik 2010 - Video</Link>
+      <Link to={`https://www.youtube.com/watch?v=gkrABIIaXfo`}>SOLO EXHIBITION in Pernik 2013 - Video</Link>
       </div>
     </main>
+
 
   )
 

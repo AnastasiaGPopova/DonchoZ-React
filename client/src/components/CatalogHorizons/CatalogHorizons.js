2d408756d0a23420ae08af6c91fddf91f71e60ff
unknown <ana.popova.register@gmail.com>
2024-04-07 21:35:41 +0300

    Update CatalogHorizons.js

--- client/src/components/CatalogHorizons/CatalogHorizons.js
+++ client/src/components/CatalogHorizons/CatalogHorizons.js
@@ -17,8 +17,8 @@ function CatalogHorizons() {
     <main className={styles.CatalogMain} >
       <div className={styles.catalogContainer1}>
         <h2>Horizons series</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        qqrqerqerq</p>
+        <p>Experience the tranquil allure of abstract horizons with a mesmerizing collection of paintings. Embracing bold minimalism and a deep color palette, each piece captivates with its peaceful landscapes or seascape vistas. Rejecting realism, the artist delves into the abstract nuances of light, invoking emotional depth and contemplation. Explore the profound impact of these artworks, where simplicity meets profundity in a symphony of color and form exploring the abstract features of the light as an emotional trigger.
+        </p>
 
          <div className={styles.gallery}>
             {paintingsHorizonts.map((x, index) => {

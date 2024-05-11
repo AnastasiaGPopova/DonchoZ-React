2625aaf5faaa68ed9b706b6e49ce87a1553b2068
unknown <ana.popova.register@gmail.com>
2024-05-11 20:06:53 +0300

    Update Homepage.js

--- client/src/components/Homepage/Homepage.js
+++ client/src/components/Homepage/Homepage.js
@@ -9,9 +9,7 @@ function Homepage() {
 
   return (
     <main className={styles.homePageMain}>
-      <div>
           <img src="https://drive.google.com/thumbnail?id=1uShljueie9gG8qZsyBjpptpg0R_vuTRN&sz=w1000" alt="test"/>
-      </div>
     </main>
 
   )

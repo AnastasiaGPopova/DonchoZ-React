a2c78994fe50c33ff60d69ea15934f8dd845e0dd
unknown <ana.popova.register@gmail.com>
2024-05-11 12:50:03 +0300

    added spinner to the catalogs

--- client/src/components/CatalogAbstract/CatalogAbstract.js
+++ client/src/components/CatalogAbstract/CatalogAbstract.js
@@ -1,42 +1,49 @@
 import styles from "./CatalogAbstract.module.css";
 import { useNavigate } from "react-router-dom";
-import {useEffect, useState } from "react";
-import * as data from '../../api/data';
+import { useEffect, useState } from "react";
+import * as data from "../../api/data";
 import { PaintingsContext } from "../../contexts/PaintingsContext";
 import { useContext } from "react";
 import SinglePainting from "../SinglePainting/SinglePainting";
-
-
+import Spinner from "../../utils/Spinner";
 
 function CatalogAbstract() {
+  const { paintingsAbstract, loading, setLoading } = useContext(
+    PaintingsContext
+  );
 
-    const {paintingsAbstract, loading , setLoading} = useContext(PaintingsContext)
-
-
-    console.log(paintingsAbstract)
+  console.log(paintingsAbstract);
 
   return (
-<main className={styles.CatalogMain} >
-  <div className={styles.catalogContainer1}>
-    <h2>Abstract series</h2>
-    <p>Vivid colors, bold contrasts, and emotive compositions define this collection. Guided by intuition, each piece explores psychological dynamics through stark contrasts, but also subtle harmonies. Engage with raw authenticity and embark on a journey of self-discovery through the vivid energy of abstract expressionism.</p>
+    <main className={styles.CatalogMain}>
+      <div className={styles.catalogContainer1}>
+        <h2>Abstract series</h2>
+        <p>
+          Vivid colors, bold contrasts, and emotive compositions define this
+          collection. Guided by intuition, each piece explores psychological
+          dynamics through stark contrasts, but also subtle harmonies. Engage
+          with raw authenticity and embark on a journey of self-discovery
+          through the vivid energy of abstract expressionism.
+        </p>
 
-    <div className={styles.gallery}>
-      {paintingsAbstract.map((x, index) => {
-        const paintingWithIndex = { ...x, index };
-        return (
-          <div key={x._id} className={styles.singlePainting}>
-            <SinglePainting {...paintingWithIndex} />
+        {/* Conditionally render the spinner only if loading is true */}
+        {loading ? (
+          <Spinner />
+        ) : (
+          <div className={styles.gallery}>
+            {paintingsAbstract.map((x, index) => {
+              const paintingWithIndex = { ...x, index };
+              return (
+                <div key={x._id} className={styles.singlePainting}>
+                  <SinglePainting {...paintingWithIndex} />
+                </div>
+              );
+            })}
           </div>
-        );
-      })}
-    </div>
-
-  </div>
-</main>
-
-  )
-
+        )}
+      </div>
+    </main>
+  );
 }
 
 export default CatalogAbstract;
--- client/src/components/CatalogOthers/CatalogOthers.js
+++ client/src/components/CatalogOthers/CatalogOthers.js
@@ -1,27 +1,26 @@
 import styles from "./CatalogOthers.module.css";
 import { useNavigate } from "react-router-dom";
-import {useEffect, useState } from "react";
-import * as data from '../../api/data';
+import { useEffect, useState } from "react";
+import * as data from "../../api/data";
 import { PaintingsContext } from "../../contexts/PaintingsContext";
 import { useContext } from "react";
 import SinglePainting from "../SinglePainting/SinglePainting";
-
-
+import Spinner from "../../utils/Spinner";
 
 function CatalogOthers() {
-
-    const {paintingsOthers, loading , setLoading} = useContext(PaintingsContext)
-
-
-    console.log(paintingsOthers)
+  const { paintingsOthers, loading, setLoading } = useContext(PaintingsContext);
 
   return (
-    <main className={styles.CatalogMain} >
+    <main className={styles.CatalogMain}>
       <div className={styles.catalogContainer1}>
         <h2>Other work</h2>
         <p></p>
 
-         <div className={styles.gallery}>
+        {/* Conditionally render the spinner only if loading is true */}
+        {loading ? (
+          <Spinner />
+        ) : (
+          <div className={styles.gallery}>
             {paintingsOthers.map((x, index) => {
               const paintingWithIndex = { ...x, index };
               return (
@@ -31,12 +30,10 @@ function CatalogOthers() {
               );
             })}
           </div>
-
+        )}
       </div>
     </main>
-
-  )
-
+  );
 }
 
 export default CatalogOthers;
